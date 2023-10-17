5965ecda2eecfc8c167b933c7caba85edd30a5c2
Russ-S <rstanleym74@gmail.com>
2023-10-17 16:13:55 +0000

    build: [US4] create Job model
    Create jobs app, create choices.py for job_type and status choices

---
+++ jobs/choices.py
@@ -0,0 +1,13 @@
+JOB_TYPE_CHOICES = (
+    ('Major Service', 'Major Service'),
+    ('Minor Service', 'Minor Service'),
+    ('MOT', 'MOT'),
+    ('Tyre Change', 'Tyre Change')
+)
+
+STATUS_CHOICES = (
+    ('Pending', 'Pending'),
+    ('Underway', 'Underway'),
+    ('Completed', 'Completed'),
+    ('Overdue', 'Overdue')
+)
---
+++ jobs/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .choices import (
+    JOB_TYPE_CHOICES,
+    STATUS_CHOICES
+)
+
+
+class Job(models.Model):
+    """
+    Job model relates to a specific owner / user instance.
+    A default image is set to reference image.url.
+    Choises are defined for job_type and status
+
+    """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    job_type = models.CharField(max_length=75, choices=JOB_TYPE_CHOICES)
+    job_details = models.TextField(blank=True)
+    status = models.CharField(max_length=75, choices=STATUS_CHOICES)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    image = models.ImageField(
+        upload_to='images/',
+        default='../default_job',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
