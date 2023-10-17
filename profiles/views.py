de6b09d98cb16ebede92a986ef7653e19e5f5710
Russ-S <rstanleym74@gmail.com>
2023-10-17 10:30:00 +0000

    build: create ProfileDetail view
    Retrieve details of a single profile based on its primary key (pk) from the Profile model, or raise a 404 error if it does no exist.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -12,6 +13,24 @@ class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    Retrieve details of a single profile based on its primary key (pk) from
+    the Profile model, or raise a 404 error if it does no exist.
+    """
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self,request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
         return Response(serializer.data)
 
 
