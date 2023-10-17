fc0edff90cc836e304adcb299a8baeece4181f13
Russ-S <rstanleym74@gmail.com>
2023-10-17 11:01:46 +0000

    build: add get and put methods to ProfileDetail view
    Add to url pattern, explictly set serilaizer_class to render form, and test profile details can be updated

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -20,7 +21,10 @@ class ProfileDetail(APIView):
     """
     Retrieve details of a single profile based on its primary key (pk) from
     the Profile model, or raise a 404 error if it does no exist.
+    Defines a get method to handle HTTP requests and a put method to handle updates to profile info.
+    serilaizer_class explicitly set to render form in preview.
     """
+    serializer_class = ProfileSerializer
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -33,4 +37,11 @@ class ProfileDetail(APIView):
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
