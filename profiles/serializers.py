012744bade80c9eec99588a72d79e58e7e2ec9cd
Russ-S <rstanleym74@gmail.com>
2023-10-17 12:06:40 +0000

    build: add is_owner serializer
    def get_is_owner, add request context to ProfileSerializer, update meta fields

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,9 +7,14 @@ class ProfileSerializer(serializers.ModelSerializer):
     Serializes specific Profile model fields into JSON data
     """
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'name', 'bio', 'image', 'created_at', 'updated_at'
+            'id', 'owner', 'name', 'bio', 'image', 'created_at', 'updated_at', 'is_owner'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -14,7 +14,7 @@ class ProfileList(APIView):
     """
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
         return Response(serializer.data)
 
 
@@ -41,12 +41,12 @@ class ProfileDetail(APIView):
 
     def get(self,request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(profile, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
