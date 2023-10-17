5e643e5cdffb936283df46ae85a2eca945932db7
Russ-S <rstanleym74@gmail.com>
2023-10-17 06:18:22 +0000

    build: install djangorestframework
    Update settings, create ProfileList view, create profile urls.py, add to main urls.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(APIView):
+    """
+    View to handle HTTP GET requests for retrieving a list of
+    profiles from the Profile Model, returning them as a JSON response.
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
+
+
