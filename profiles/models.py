c1eaa6595d620656004e2cb815bcb01246ca4cfd
Russ-S <rstanleym74@gmail.com>
2023-10-17 05:14:25 +0000

    build: [US3] create profile model
    Utilises the post_save signal to create a profile instance  when a new User is created.

---
+++ profiles/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    """
+    Model for creating a user profile with additional fields.
+    """
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['_created_at']
+
+    def__str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    """
+    Utilises the post_save signal to create a profile instance
+    when a new User is created.
+    """
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
