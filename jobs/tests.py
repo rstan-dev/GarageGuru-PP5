8d5876e2ad2cad8c2c273cac832541c88e57fd8b
Russ-S <rstanleym74@gmail.com>
2023-10-18 15:04:39 +0000

    test: write APITestCase for JobListView
    test_user_can_retrieve_all_jobs

--- jobs/tests.py
+++ jobs/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Job
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class JobListViewTests(APITestCase):
+    def setUp(self):
+        testuser1 = User.objects.create_user(username='testuser1', password='testpw1234')
+        testuser2 = User.objects.create_user(username='testuser2', password='testpw1234')
+
+    def test_user_can_retrieve_all_jobs(self):
+        testuser1 = User.objects.get(username='testuser1')
+        testuser2 = User.objects.get(username='testuser2')
+        Job.objects.create(
+            owner=testuser1,
+            job_type='Service',
+            job_details='test details',
+            status='Pending',
+            assigned_to=testuser2
+        )
+        response = self.client.get('/jobs/')
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
