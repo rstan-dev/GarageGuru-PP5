917d8c203611f41d6de0429257eb5b2ff18f4aa4
Russ-S <rstanleym74@gmail.com>
2023-10-18 09:31:08 +0000

    build: add validate_image() to JobSerializer

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -6,13 +6,34 @@ from .models import Job
 class JobSerializer(serializers.ModelSerializer):
     """
     Serializes specific Job model fields into JSON data.
-    assigned_to variable 
-    get_is_owner method checks if the user making the request is the owner of the profile.
+
+    assigned_to variable returns username dropdown.
+
+    validate_image uses validation function to manage image size.
+
+    get_is_owner method checks if the user making the request is the owner of
+    the profile.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
     assigned_to = serializers.StringRelatedField()
+
+    def validate_image(self, value):
+        "check if file size is greater than 2mb"
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2mb!'
+            )
+        if value.image.width > 2048:
+            raise serializers.ValidationError(
+                'Image width larger than 2048px!'
+            )
+        if value.image.height > 2048:
+            raise serializers.ValidationError(
+                'Image height larger than 2048px!'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -23,5 +44,5 @@ class JobSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'assigned_to', 'job_type',
             'job_details', 'status', 'created_at', 'updated_at',
-            'image', 'is_owner'
+            'image', 'is_owner', 'image_filter',
         ]
