b0e6f1b92a7ef58d000cb41cbefa3ee022166641
Russ-S <rstanleym74@gmail.com>
2023-10-18 15:46:21 +0000

    fix: testcase for creatin a job
    Fix typos, assigned_to, indentation, confrim 2 tests run OK

--- jobs/tests.py
+++ jobs/tests.py
@@ -11,13 +11,13 @@ class JobListViewTests(APITestCase):
         testuser2 = User.objects.create_user(username='testuser2', password='testpw1234')
 
     def test_user_can_retrieve_all_jobs(self):
-    """
-    Test that a user can retrieve a list of all jobs.
+        """
+        Test that a user can retrieve a list of all jobs.
 
-    This test checks if a user, after logging in, can successfully retrieve a list of all jobs.
-    It creates a sample job in the database and then sends a GET request to the jobs endpoint
-    to fetch the list of jobs. The test verifies that the HTTP response status is 200 (OK)
-    """
+        This test checks if a user, after logging in, can successfully retrieve a list of all jobs.
+        It creates a sample job in the database and then sends a GET request to the jobs endpoint
+        to fetch the list of jobs. The test verifies that the HTTP response status is 200 (OK)
+        """
         testuser1 = User.objects.get(username='testuser1')
         testuser2 = User.objects.get(username='testuser2')
         Job.objects.create(
@@ -32,7 +32,7 @@ class JobListViewTests(APITestCase):
             response.status_code,
             status.HTTP_200_OK)
 
-        def test_logged_in_user_can_create_job(self):
+    def test_logged_in_user_can_create_job(self):
         """
         Test that a logged-in user can create a job.
 
@@ -41,20 +41,22 @@ class JobListViewTests(APITestCase):
         is created in the database and that the HTTP response status is
         201 (Created).
         """
-            self.client.login(username='testuser1', password='testpw1234')
-            testuser2 = User.objects.get(username='testuser2')
-            response = self.client.post(
-                '/jobs/',
-                {
-                    'job_type': 'Service',
-                    'job_details': 'test details',
-                    'status': 'Pending',
-                    'assigned_to': testuser2
-                }
-            )
-            count = Job.objects.count()
-            self.assertEqual(count, 1)
-            self.assertEqual(
-                response.status_code,
-                status.HTTP_201_CREATED)
+        self.client.login(username='testuser1', password='testpw1234')
+        testuser2 = User.objects.get(username='testuser2')
+        response = self.client.post(
+            '/jobs/',
+            {
+                'job_type': 'Major Service',
+                'job_details': 'test details',
+                'status': 'Pending',
+                'assigned_to': testuser2.id
+            }
+        )
+        count = Job.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED)
+       
+
 
