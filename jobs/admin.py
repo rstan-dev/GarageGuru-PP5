50308461b0daae9a90140b34c9cf6bc32750cab7
Russ-S <rstanleym74@gmail.com>
2023-10-18 08:19:02 +0000

    build: add admin decorator for JobAdmin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Job
 
-admin.site.register(Job)
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'updated_at', 'job_type', 'owner', 'assigned_to', 'status',)
+    list_display_links = ('job_type',)
