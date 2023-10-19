7cb702985aa06e2527172ed729927ca7dcb367ec
Russ-S <rstanleym74@gmail.com>
2023-10-19 12:09:41 +0000

    refactor: ProfileDetailView with generics RetrieveUpdate, run testsas OK

--- profiles/views.py
+++ profiles/views.py
@@ -17,40 +17,13 @@ class ProfileList(generics.ListAPIView):
     queryset = Profile.objects.all()
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
-    Retrieve details of a single profile based on its primary key (pk) from
-    the Profile model, or raise a 404 error if it does no exist.
-
-    Defines a get method to handle HTTP requests and a put method to handle
-    updates to profile info.
-
-    serializer_class explicitly set to render form in preview.
-
-    permission_classes set for isAurthenticated and IsOwnerOrReadOnly ensures user making the
-    request has the correct permissions.
+    View all profiles if logged in
+    Edit a profile if owner is logged in or view profile if
+    not the owner.
+    Utilises the generics RetrieveUpdateAPI view.
     """
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self,request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    queryset = Profile.objects.all()
