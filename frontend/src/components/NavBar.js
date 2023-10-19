71a772eb4025d6fe59cbbe3178389cca3df7c78c
Russ-S <rstanleym74@gmail.com>
2023-10-19 16:24:08 +0000

    feat: NavBar custom css, update NavLinks

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,42 +1,43 @@
 import React from "react";
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from "../assets/garageguru_logo.png"
+import styles from "../styles/NavBar.module.css"
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" fixed="top">
+    <Navbar  className={styles.NavBar} expand="lg" fixed="top">
         <Container>
             <Navbar.Brand>
                 <img src={logo} alt="logo" height="50" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link >
+                <Nav className="ml-auto">
+                <Nav.Link className={styles.NavLinks} >
                     <i class="fa-solid fa-car"></i>  All Jobs
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks}>
                     <i class="fa-solid fa-clipboard-list"></i> My Jobs
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks}>
                     <i class="fa-solid fa-circle-plus"></i> Add Job
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks}>
                     <i class="fa-solid fa-people-group"></i> Assigned
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks} >
                     <i class="fa-solid fa-eye"></i> Watching
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks}>
                     <i class="fa-solid fa-right-to-bracket"></i> Login
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks}>
                     <i class="fa-solid fa-right-from-bracket"></i> Logout
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks}>
                     <i class="fa-solid fa-user-plus"></i> Register
                 </Nav.Link>
-                <Nav.Link >
+                <Nav.Link className={styles.NavLinks}>
                     <i class="fa-solid fa-circle-user"></i> Profile
                 </Nav.Link>
                 </Nav>
