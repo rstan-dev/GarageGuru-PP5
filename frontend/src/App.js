70b9ab9e61dc4e515911328c265984aa1500f86f
Russ-S <rstanleym74@gmail.com>
2023-10-23 09:15:11 +0000

    build: install axios and set axiosDefaults

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import PageNotFound from './components/PageNotFound';
 import { Container } from 'react-bootstrap';
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch } from "react-router-dom";
+import './api/axiosDefaults';
 
 
 function App() {
---
+++ frontend/src/api/axiosDefaults.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+
+axios.defaults.baseURL = 'https://3000-rstandev-garagegurupp5-g87jnb0h38p.ws-eu105.gitpod.io/';
+axios.defaults.headers.post['Content-type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
+
