3ed565dd6512ec5fbf9f5ac0e6b798717dd9e59e
Russ-S <rstanleym74@gmail.com>
2023-10-23 14:11:54 +0000

    build: install all-auth
    Update settings, install simplejwt, create UserProfileSerializer, migrate db, freeze requirements.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -27,6 +27,23 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.UserProfileSerializer'
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -54,9 +71,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
     'profiles',
     'jobs',
 ]
+
+SITE_ID =1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- frontend/src/pages/auth/LoginForm.js
+++ frontend/src/pages/auth/LoginForm.js
@@ -1,10 +1,12 @@
-import { React, useState } from "react";
+import { React, useState} from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import axios from "axios";
 
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 
 import styles from "../../styles/LoginRegister.module.css"
 
@@ -17,6 +19,8 @@ function LoginForm() {
 
     const {username, password} = logInData;
 
+    const [errors, setErrors] = useState()
+
     const handleChange = (event) => {
         setlogInData({
             ...logInData,
@@ -25,12 +29,11 @@ function LoginForm() {
     }
 
 
-
   return (
     <Container className={styles.LoginRegisterForm}>
         <Col xs={12} sm={12} md={8} lg={6} xl={6} className="mx-auto">
             <h1> Log In</h1>
-            <Form>
+            <Form >
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
                     <Form.Control
@@ -50,6 +53,7 @@ function LoginForm() {
                         value={password}
                         onChange={handleChange} />
                 </Form.Group>
+
                 <Button variant="primary" type="submit">
                     Log In
                 </Button>
