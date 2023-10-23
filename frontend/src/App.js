fbf4e26618a0ff2f842b59d5b38c92695a7d8c74
Russ-S <rstanleym74@gmail.com>
2023-10-23 14:47:08 +0000

    build: create CurrentUserContext in app.js
    Wrap around Route Container to make availabe to all child components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,30 +6,53 @@ import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults';
 
 import LoginForm from './pages/auth/LoginForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const handleMount = async () => {
+    try {
+      const {data} = await axios.get('dj-rest-auth/user')
+      setCurrentUser(data)
+    } catch(err) {
+      console.log(err)
+
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+  }, [])
+
+
   return (
-    <div className={styles.App}>
-      < NavBar />
-      < Container className={styles.Content}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>All Jobs</h1>} />
-          <Route exact path="/myjobs" render={() => <h1>My Jobs</h1>} />
-          <Route exact path="/addjob" render={() => <h1>Add Job</h1>} />
-          <Route exact path="/assigned" render={() => <h1>Assigned Jobs</h1>} />
-          <Route exact path="/watched" render={() => <h1>Watched Jobs</h1>} />
-          <Route exact path="/login" render={() => <LoginForm />} />
-          <Route exact path="/logout" render={() => <h1>Logout</h1>} />
-          <Route exact path="/register" render={() => <h1>Register</h1>} />
-          <Route exact path="/profile" render={() => <h1>Profile</h1>} />
-          <Route render={() => <PageNotFound />} />
-      </Switch>
-
-
-
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider vlaue={setCurrentUser}>
+        <div className={styles.App}>
+          < NavBar />
+          < Container className={styles.Content}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>All Jobs</h1>} />
+              <Route exact path="/myjobs" render={() => <h1>My Jobs</h1>} />
+              <Route exact path="/addjob" render={() => <h1>Add Job</h1>} />
+              <Route exact path="/assigned" render={() => <h1>Assigned Jobs</h1>} />
+              <Route exact path="/watched" render={() => <h1>Watched Jobs</h1>} />
+              <Route exact path="/login" render={() => <LoginForm />} />
+              <Route exact path="/logout" render={() => <h1>Logout</h1>} />
+              <Route exact path="/register" render={() => <h1>Register</h1>} />
+              <Route exact path="/profile" render={() => <h1>Profile</h1>} />
+              <Route render={() => <PageNotFound />} />
+          </Switch>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
