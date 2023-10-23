0a0204bc4a6249cb1056b548d7e816ff36a3078d
Russ-S <rstanleym74@gmail.com>
2023-10-23 14:32:43 +0000

    build: add handleSubmit to Login form
    Add bootstrap alerts to form fields

--- frontend/src/pages/auth/LoginForm.js
+++ frontend/src/pages/auth/LoginForm.js
@@ -21,6 +21,8 @@ function LoginForm() {
 
     const [errors, setErrors] = useState()
 
+    const history = useHistory()
+
     const handleChange = (event) => {
         setlogInData({
             ...logInData,
@@ -28,41 +30,68 @@ function LoginForm() {
         })
     }
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/login/',
+            logInData);
+            history.push('/');
+        } catch (err) {
+            setErrors(err.response?.data);
 
-  return (
-    <Container className={styles.LoginRegisterForm}>
-        <Col xs={12} sm={12} md={8} lg={6} xl={6} className="mx-auto">
-            <h1> Log In</h1>
-            <Form >
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter your username"
-                        name="username"
-                        value={username}
-                        onChange={handleChange}/>
-                </Form.Group>
+        }
+    };
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Enter Password"
-                        name="password"
-                        value={password}
-                        onChange={handleChange} />
-                </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Log In
-                </Button>
+    return (
+        <Container className={styles.LoginRegisterForm}>
+            <Col xs={12} sm={12} md={8} lg={6} xl={6} className="mx-auto">
+                <h1> Log In</h1>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter your username"
+                            name="username"
+                            value={username}
+                            onChange={handleChange}/>
+                    </Form.Group>
+                    {errors.username?.map((message, index) => (
+                        <Alert key={index} variant="warning">
+                            {message}
+                        </Alert>
+                    ))}
 
-                <p>Don't have an account?<br/>Click here to register</p>
-            </Form>
-        </Col>
-    </Container>
-  )
-}
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            value={password}
+                            onChange={handleChange} />
+                    </Form.Group>
+                    {errors.password?.map((message, index) => (
+                        <Alert key={index} variant="warning">
+                            {message}
+                        </Alert>
+                    ))}
+
+                    <Button variant="primary" type="submit">
+                        Log In
+                    </Button>
+                    {errors.non_field_errors?.map((message, idx) => (
+                        <Alert variant="warning" key={idx} className="mt-3">
+                        {message}
+                        </Alert>
+                    ))}
+
+                    <p>Don't have an account?<br/>Click here to register</p>
+                </Form>
+            </Col>
+        </Container>
+    )
+    }
 
 export default LoginForm
