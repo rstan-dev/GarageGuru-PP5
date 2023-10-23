211627472dd56e02a440f6ae90ae0a7e2c22ebb5
Russ-S <rstanleym74@gmail.com>
2023-10-23 11:21:14 +0000

    style: add LoginRegisterForm css

--- frontend/src/pages/auth/LoginForm.js
+++ frontend/src/pages/auth/LoginForm.js
@@ -8,40 +8,38 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 import appStyles from "../../App.module.css";
+import styles from "../../styles/LoginRegister.module.css"
 
 
 function LoginForm() {
   return (
-    <Row>
-        <Col>
-            <Container>
-                <h1> Log In</h1>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control
-                          type="text"
-                          placeholder="Enter your username"
-                          name="username"/>
-                    </Form.Group>
+    <Container className={styles.LoginRegisterForm}>
+        <Col xs={12} sm={12} md={8} lg={6} xl={6} className="mx-auto">
+            <h1> Log In</h1>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter your username"
+                        name="username"/>
+                </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          placeholder="Enter Password"
-                          name="password" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Log In
-                    </Button>
-                </Form>
-            </Container>
-            <Container>
-                <h2>Don't have an account?  Click here to register</h2>
-            </Container>
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter Password"
+                        name="password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Log In
+                </Button>
+
+                <p>Don't have an account?<br/>Click here to register</p>
+            </Form>
         </Col>
-    </Row>
+    </Container>
   )
 }
 
