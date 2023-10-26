a858a2502fa59c37c09ca3ede815b0208e055669
Russ-S <rstanleym74@gmail.com>
2023-10-26 11:26:30 +0000

    iteration: Add submit functionality to Register Form
    Create handleSubmit(), add useHistory for redirect, add errors state, add Alert messages under form fields - test on blank form

--- frontend/src/pages/auth/RegisterForm.js
+++ frontend/src/pages/auth/RegisterForm.js
@@ -4,8 +4,11 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { Alert } from "react-bootstrap";
 
 import styles from "../../styles/LoginRegister.module.css"
+import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function RegisterForm() {
@@ -17,6 +20,12 @@ function RegisterForm() {
 
     const {username, password1, password2} = registerData;
 
+    const [errors, setErrors] = useState({})
+
+    const history = useHistory
+
+
+
     const handleChange = (event) => {
         setRegisterData({
             ...registerData,
@@ -24,12 +33,23 @@ function RegisterForm() {
         });
     };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('dj-rest-auth/registration/', registerData)
+            history.push('/login')
+
+        } catch(error) {
+            setErrors(error.response?.data)
+        }
+    }
+
 
     return (
         <Container className={styles.LoginRegisterForm}>
             <Col xs={12} sm={12} md={8} lg={6} xl={6} className="mx-auto">
                 <h1>Register for an account</h1>
-                <Form >
+                <Form onSubmit={handleSubmit}>
                     <Form.Group controlId="username">
                         <Form.Label>Username</Form.Label>
                         <Form.Control
@@ -40,6 +60,11 @@ function RegisterForm() {
                             onChange={handleChange}
                             />
                     </Form.Group>
+                    {errors.username?.map((message, index) => (
+                    <Alert variant="warning" key={index}>
+                    {message}
+                    </Alert>
+                    ))}
 
                     <Form.Group controlId="password1">
                         <Form.Label>Password</Form.Label>
@@ -51,6 +76,11 @@ function RegisterForm() {
                             onChange={handleChange}
                              />
                     </Form.Group>
+                    {errors.password1?.map((message, index) => (
+                    <Alert variant="warning" key={index}>
+                    {message}
+                    </Alert>
+                    ))}
 
                     <Form.Group controlId="password2">
                         <Form.Label>Confirm Password</Form.Label>
@@ -62,13 +92,22 @@ function RegisterForm() {
                             onChange={handleChange}
                              />
                     </Form.Group>
+                    {errors.password2?.map((message, index) => (
+                    <Alert variant="warning" key={index}>
+                    {message}
+                    </Alert>
+                    ))}
 
                     <Button variant="primary" type="submit">
                         Register
                     </Button>
+                    {errors.non_field_errors?.map((message, index) => (
+                        <Alert variant="warning" key={index} className="mt-3">
+                        {message}
+                        </Alert>
+                    ))}
 
                     <p>Already have an account?<br/>Click here to log in</p>
-
                 </Form>
             </Col>
         </Container>
