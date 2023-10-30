9774d1392bad65886683f4383fe8f342b2a9c511
Russ-S <rstanleym74@gmail.com>
2023-10-30 15:04:39 +0000

    iteration: add change password functionality
    Add userdata state.  Add handleChange, handleSubmit, useEffect and useCurrentUser.
    
    Update form attributes.  Add Errors and Alerts. Manually test password update

--- frontend/src/pages/profile/ChangePasswordForm.js
+++ frontend/src/pages/profile/ChangePasswordForm.js
@@ -1,38 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefaults";
+import { useHistory } from "react-router-dom";
 
 
 const ChangePasswordForm = () => {
+    const history = useHistory();
+    const currentUser = useCurrentUser();
+
+    const [userData, setUserData] = useState({
+      new_password1: "",
+      new_password2: "",
+    });
+    const { new_password1, new_password2 } = userData;
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (event) => {
+        setUserData({
+          ...userData,
+          [event.target.name]: event.target.value,
+        });
+      };
+
+    useEffect(() => {
+    if (!currentUser) {
+      // redirect user if they are not the owner of this profile
+      history.push("/login");
+    }
+    }, [currentUser, history]);
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        await axiosRes.post("/dj-rest-auth/password/change/", userData);
+        history.goBack();
+      } catch (err) {
+        console.log(err);
+        console.log(userData)
+        setErrors(err.response?.data);
+
+      }
+    };
 
   return (
     <Row>
       <Col className="py-2 mx-auto text-center" md={6}>
         <Container >
-          <Form >
+          <Form onSubmit={handleSubmit}>
             <Form.Group>
-              <Form.Label>New password</Form.Label>
+            {errors?.new_password1?.map((message, index) => (
+              <Alert key={index} variant="warning">
+                {message}
+              </Alert>
+            ))}
+              <Form.Label>Update password</Form.Label>
               <Form.Control
-                placeholder="new password"
+                placeholder="choose a new password"
                 type="password"
                 name="new_password1"
+                value={new_password1}
+                onChange={handleChange}
               />
             </Form.Group>
 
             <Form.Group>
+            {errors?.new_password2?.map((message, index) => (
+              <Alert key={index} variant="warning">
+                {message}
+              </Alert>
+            ))}
               <Form.Label>Confirm password</Form.Label>
               <Form.Control
                 placeholder="confirm new password"
                 type="password"
                 name="new_password2"
+                value={new_password2}
+                onChange={handleChange}
               />
             </Form.Group>
 
             <Button
+                onClick={() => history.goBack()}
             >
               Cancel
             </Button>
