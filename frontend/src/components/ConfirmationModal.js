7c63cd146d5b9d78d11b7da52c8a427cab4abd4a
Russ-S <rstanleym74@gmail.com>
2023-11-09 12:50:10 +0000

    iteration: Add Confirmation Modal to EditJob
    Create ConfirmationModal component
    
    Add showConfirmationModal state to EditJobForm.
    
    Update handleSubmit to show and close modal.
    
    Update Form onSubmit and Update Job button.  Add Confirmation Modal component after Form. Test with updated
    form.

---
+++ frontend/src/components/ConfirmationModal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const ConfirmationModal = ({ showModal, handleClose, handleConfirm, title, body }) => {
+    // Reusable Modal to offer a user the chance to Confirm or Cancel the request
+  return (
+    <Modal show={showModal} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{body}</Modal.Body>
+      <Modal.Footer>
+        <Button variant="warning" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="success" onClick={handleConfirm}>
+          Confirm
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
