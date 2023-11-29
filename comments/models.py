cc0c6cea092b450fc823951a206fd05ac23b3da1
Russ-S <rstanleym74@gmail.com>
2023-11-29 11:32:57 +0000

    enhancement: add comment reply functionality
    Add parent self-referential FK to CommentModel. Migrate DB.
    
    Add parent and replies fields to CommentSerializer.
    
    Create CommentReplySerializer to distinguish field names and avoid further nesting of comments.
    
    Add custom get_queryset in views.py.
    
    Test in drf backend view that comment replies are posted and appearing as expected.

--- comments/models.py
+++ comments/models.py
@@ -5,10 +5,12 @@ from jobs.models import Job
 
 class Comment(models.Model):
     """
-    Comment model, related to User and Job
+    Comment model, related to User and Job.
+    Parent self reference allows for replies to comments.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, related_name='comments', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     comment_detail = models.TextField()
--- comments/serializers.py
+++ comments/serializers.py
@@ -6,7 +6,11 @@ from comments.models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     """
     Serializer for the Comment model
-    Adds three extra fields for is_owner, profile id and profile image, when returning each comment.
+    Adds three extra fields for is_owner, profile id and profile image, when
+    returning each comment.
+
+    Adds parent field for handling the id of the parent comment
+    Adds replies field for handling replies to specific comments
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     created_at = serializers.SerializerMethodField()
@@ -15,6 +19,11 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    parent = serializers.PrimaryKeyRelatedField(queryset=Comment.objects.all(),
+                                                required=False)
+    replies = serializers.SerializerMethodField(method_name='get_replies')
+
 
 
     def get_is_owner(self, obj):
@@ -27,11 +36,24 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
 
+    def get_replies(self, obj):
+        """
+        Serialize replies without nesting further replies
+        Only get replies for top-level comments
+        No nested replies
+        """
+        if obj.parent is None:
+            replies = Comment.objects.filter(parent=obj)
+            return CommentReplySerializer(replies, many=True,
+                                        context=self.context).data
+        return []
+
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'job', 'comment_detail', 'created_at', 'updated_at',
-            'is_owner', 'profile_id', 'profile_image',
+            'id', 'owner', 'job', 'comment_detail', 'created_at',
+            'updated_at', 'is_owner', 'profile_id', 'profile_image',
+            'parent', 'replies',
         ]
 
 
@@ -41,3 +63,25 @@ class CommentDetailSerializer(CommentSerializer):
     with a specific job
     """
     job = serializers.ReadOnlyField(source='job.id')
+
+
+class CommentReplySerializer(serializers.ModelSerializer):
+    """
+    Simplified serializer for replies, excludes parent and replies to avoid
+    further nesting
+    """
+    reply_id = serializers.IntegerField(source='id', read_only=True)
+    reply_owner = serializers.ReadOnlyField(source='owner.username')
+    reply_comment_detail = serializers.ReadOnlyField(source='comment_detail')
+    reply_created_at = serializers.SerializerMethodField()
+    reply_updated_at = serializers.SerializerMethodField()
+
+    def get_reply_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_reply_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Comment
+        fields = ['reply_id', 'reply_owner', 'reply_comment_detail', 'reply_created_at', 'reply_updated_at']
