38336ec37a9f278ea60e402c25c3a1387fe0c1f9
Russ-S <rstanleym74@gmail.com>
2023-12-04 19:20:50 +0000

    style: format frontend files
    Format with Prettier formatter extension, update code comments.
    
    Update axiosDefaults, Asset, ConfirmationModal, FixedHeader, NavBar, PageNotFound, PageNotFoundLoggedOut.

--- frontend/src/components/Asset.js
+++ frontend/src/components/Asset.js
@@ -3,14 +3,13 @@ import { Spinner } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
 
 const Asset = ({ spinner, icon, message }) => {
-
-    return (
-    <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
-      {icon && <i className={icon}></i>}
-      {message && <p>{message}</p>}
-    </div>
-  );
+	return (
+		<div className={`${styles.Asset} p-4`}>
+			{spinner && <Spinner animation='border' />}
+			{icon && <i className={icon}></i>}
+			{message && <p>{message}</p>}
+		</div>
+	);
 };
 
 export default Asset;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,132 +1,166 @@
 import React from "react";
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from "../assets/garageguru_logo.jpg"
-import styles from "../styles/NavBar.module.css"
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/garageguru_logo.jpg";
+import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import {
+	useCurrentUser,
+	useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
 import useToggleMenu from "../hooks/useToggleMenu";
 import axios from "axios";
-import { removeTokenTimestamp } from '../utils/utils';
+import { removeTokenTimestamp } from "../utils/utils";
 
-
+/**
+ * NavBar Component
+ *
+ * This component renders the navigation bar for the application. It displays different
+ * navigation links based on whether a user is currently logged in or not.
+ **/
 const NavBar = () => {
-    const currentUser = useCurrentUser();
-    const setCurrentUser = useSetCurrentUser();
-    const currentUserId = currentUser?currentUser.pk : null;
+	const currentUser = useCurrentUser();
+	const setCurrentUser = useSetCurrentUser();
+	const currentUserId = currentUser ? currentUser.pk : null;
 
-    const { expanded, setExpanded, ref } = useToggleMenu();
+	const { expanded, setExpanded, ref } = useToggleMenu();
 
-    const handleLogOut = async () => {
-        try {
-            await axios.post("/dj-rest-auth/logout/");
-            setCurrentUser(null)
-            removeTokenTimestamp()
-        } catch (err) {
-            console.log(err);
-        }
-    };
+	/*
+    Handles user logout and
+    removes TokenTimestamp
+    */
+	const handleLogOut = async () => {
+		try {
+			await axios.post("/dj-rest-auth/logout/");
+			setCurrentUser(null);
+			removeTokenTimestamp();
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-    const loggedInMenu = (
-        <>
-        <NavLink to="/jobs/addjob"
-        className={`${styles.NavLinks} ${styles.AddJobIcon}`}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-circle-plus"></i>
-            <span className={styles.Label}>Add Job</span>
-        </NavLink>
-        <NavLink exact to="/"
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-car"></i>
-            <span className={styles.Label}>All Jobs</span>
-        </NavLink>
-        <NavLink to="/myjobs"
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-clipboard-list"></i>
-                <span className={styles.Label}>My Jobs</span>
-        </NavLink>
+	/*
+    Conditionally displays menu items to logged in users
+    */
+	const loggedInMenu = (
+		<>
+			<NavLink
+				to='/jobs/addjob'
+				className={`${styles.NavLinks} ${styles.AddJobIcon}`}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-circle-plus'></i>
+				<span className={styles.Label}>Add Job</span>
+			</NavLink>
+			<NavLink
+				exact
+				to='/'
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-car'></i>
+				<span className={styles.Label}>All Jobs</span>
+			</NavLink>
+			<NavLink
+				to='/myjobs'
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-clipboard-list'></i>
+				<span className={styles.Label}>My Jobs</span>
+			</NavLink>
+			<NavLink
+				to='/assigned'
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-people-group'></i>
+				<span className={styles.Label}>Assigned</span>
+			</NavLink>
+			<NavLink
+				to='/watched'
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-eye'></i>
+				<span className={styles.Label}>Watching</span>
+			</NavLink>
+			<NavLink
+				to='/all-invoices'
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-cash-register'></i>
+				<span className={styles.Label}>Invoices</span>
+			</NavLink>
+			<NavLink
+				to={"/login"}
+				className={styles.NavLinks}
+				activeClassName={styles.Active}
+				onClick={handleLogOut}>
+				<i className='fa-solid fa-power-off'></i>
+				<span className={styles.Label}>Logout</span>
+			</NavLink>
+			<NavLink
+				to={`/profile/${currentUserId}`}
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-circle-user'></i>
+				<span className={styles.Label}>Profile</span>
+				<span className={styles.Username}>
+					Welcome: {currentUser?.username}
+				</span>
+			</NavLink>
+		</>
+	);
 
-        <NavLink to="/assigned"
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-people-group"></i>
-            <span className={styles.Label}>Assigned</span>
-        </NavLink>
-        <NavLink to="/watched"
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-eye"></i>
-            <span className={styles.Label}>Watching</span>
-        </NavLink>
-        <NavLink to="/all-invoices"
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-cash-register"></i>
-            <span className={styles.Label}>Invoices</span>
-        </NavLink>
-        <NavLink
-            to={"/login"}
-            className={styles.NavLinks}
-            activeClassName={styles.Active}
-            onClick={handleLogOut}>
-            <i className="fa-solid fa-power-off"></i>
-            <span className={styles.Label}>Logout</span>
-        </NavLink>
-        <NavLink to={`/profile/${currentUserId}`}
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-circle-user"></i>
-            <span className={styles.Label}>Profile</span>
-            <span className={styles.Username}>Welcome:  {currentUser?.username}</span>
-        </NavLink>
+	/*
+    Conditionally displays menu items to logged out users
+    */
+	const loggedOutMenu = (
+		<>
+			<NavLink
+				to='/login'
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-right-to-bracket'></i>
+				<span className={styles.Label}>Login</span>
+			</NavLink>
+			<NavLink
+				to='/register'
+				className={styles.NavLinks}
+				activeClassName={styles.Active}>
+				<i className='fa-solid fa-user-plus'></i>
+				<span className={styles.Label}>Register</span>
+			</NavLink>
+		</>
+	);
 
+	return (
+		<Navbar
+			className={styles.NavBar}
+			expand='lg'
+			fixed='top'
+			expanded={expanded}>
+			<Container>
+				<NavLink to='/'>
+					<Navbar.Brand>
+						<img
+							src={logo}
+							alt='logo'
+							height='50'
+						/>
+					</Navbar.Brand>
+				</NavLink>
+				<Navbar.Toggle
+					aria-controls='basic-navbar-nav'
+					ref={ref}
+					onClick={() => setExpanded(!expanded)}
+					className='bg-light'
+				/>
+				<Navbar.Collapse
+					className={styles.NavbarCollapse}
+					id='basic-navbar-nav'>
+					<Nav className='ml-auto'>
+						{currentUser ? loggedInMenu : loggedOutMenu}
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
 
-        </>
-    )
-    const loggedOutMenu = (
-        <>
-        <NavLink to="/login"
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-right-to-bracket"></i>
-            <span className={styles.Label}>Login</span>
-        </NavLink>
-        <NavLink to="/register"
-        className={styles.NavLinks}
-        activeClassName={styles.Active}>
-            <i className="fa-solid fa-user-plus"></i>
-            <span className={styles.Label}>Register</span>
-        </NavLink>
-        </>
-    )
-
-    return (
-    <Navbar
-        className={styles.NavBar}
-        expand="lg"
-        fixed="top"
-        expanded={expanded}>
-        <Container>
-            <NavLink to="/">
-                <Navbar.Brand>
-                    <img src={logo} alt="logo" height="50" />
-                </Navbar.Brand>
-            </NavLink>
-            <Navbar.Toggle
-                aria-controls="basic-navbar-nav"
-                ref={ref}
-                onClick={() => setExpanded(!expanded)}
-                className="bg-light"/>
-            <Navbar.Collapse className={styles.NavbarCollapse} id="basic-navbar-nav">
-                <Nav className="ml-auto">
-
-                {currentUser ? loggedInMenu : loggedOutMenu}
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
-  )
-}
-
-export default NavBar
+export default NavBar;
