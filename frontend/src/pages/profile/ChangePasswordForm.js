16895777a214ff05ce32d6a1818764a66b84b16f
Russ-S <rstanleym74@gmail.com>
2023-12-04 15:22:52 +0000

    style: add style to ChangePassword page
    Create css module.  Update ChangePasswordForm.

--- frontend/src/pages/profile/ChangePasswordForm.js
+++ frontend/src/pages/profile/ChangePasswordForm.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
+import styles from "../../styles/ChangeUsernamePassword.module.css"
 
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
@@ -58,11 +60,22 @@ const ChangePasswordForm = () => {
     };
 
   return (
-    <Row>
-      <Col className="py-2 mx-auto text-center" md={6}>
-        <Container >
+    <Container className={styles.ProfileForm}>
+      <Col xs={12} sm={12} md={8} lg={8} xl={8} className="mx-auto">
+
+
+          <div className={styles.CardBlock}>
           {/* Display success message */}
           {successMessage && <Alert variant="success">{successMessage}</Alert>}
+
+          <Card className={styles.FormCard}>
+                  <div className={`d-flex flex-column align-items-center`}>
+                      <p>
+                        <i className={`fa-solid fa-pencil ${styles.EditIcon}`}>
+                  </i>
+                  Change Password
+                      </p>
+                  </div>
 
           <Form onSubmit={handleSubmit}>
             <Form.Group>
@@ -97,20 +110,30 @@ const ChangePasswordForm = () => {
               />
             </Form.Group>
 
-            <Button
-                onClick={() => history.goBack()}
+            <Row className="justify-content-center">
+            <Col md="auto" className={styles.BtnContainer}>
+                <Button
+                    onClick={() => history.goBack()}
+                    variant="warning"
             >
               Cancel
-            </Button>
-            <Button
-              type="submit"
+                </Button>
+            </Col>
+            <Col md="auto" className={styles.BtnContainer}>
+                <Button
+                    type="submit"
+                    variant="success"
             >
               Update Password
-            </Button>
-          </Form>
-        </Container>
+                </Button>
+            </Col>
+                </Row>
+            </Form>
+
+            </Card>
+        </div>
       </Col>
-    </Row>
+    </Container>
   );
 };
 
