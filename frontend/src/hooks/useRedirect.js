228d6907d8b7500a1b60bd49033f133148937ee8
Russ-S <rstanleym74@gmail.com>
2023-12-04 19:34:56 +0000

    style: format frontend contexts and hooks files
    Format with Prettier formatter extension, update code comments.
    
    Update CurrentUserContext, useRedirect, useToggleMenu.

--- frontend/src/hooks/useRedirect.js
+++ frontend/src/hooks/useRedirect.js
@@ -2,25 +2,33 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
+/**
+ * Custom hook to redirect the user based on their authentication status.
+ **/
 export const useRedirect = (userAuthStatus) => {
-    const history = useHistory();
+	const history = useHistory();
 
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                await axios.post('/dj-rest-auth/token/refresh/');
-                // if user is logged in, the code will run
-                if (userAuthStatus === 'loggedIn') {
-                    history.goBack()
-                }
-
-            } catch(err) {
-                // if user is not logged in, the code will run
-                if (userAuthStatus === 'loggedOut' ) {
-                    history.push('/login')
-                }
-            }
-        }
-        handleMount();
-    }, [history, userAuthStatus])
+	useEffect(() => {
+		/**
+		 * Handles the initial mount logic for redirection based on
+		 * the user's authentication status.
+		 */
+		const handleMount = async () => {
+			try {
+				await axios.post("/dj-rest-auth/token/refresh/");
+				// if user is logged in, the code will attempt to refresh
+				// the token and navigate back to the previous page.
+				if (userAuthStatus === "loggedIn") {
+					history.goBack();
+				}
+			} catch (err) {
+				// if there is an error, or if user is not logged in, the
+				// user will be redirected to the login page.
+				if (userAuthStatus === "loggedOut") {
+					history.push("/login");
+				}
+			}
+		};
+		handleMount();
+	}, [history, userAuthStatus]);
 };
--- frontend/src/hooks/useToggleMenu.js
+++ frontend/src/hooks/useToggleMenu.js
@@ -1,21 +1,31 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from "react";
 
+/**
+ * Custom hook for managing the toggle state of a menu.
+ *
+ * This hook provides the expanded state and a method to set this state for a menu.
+ * It also handles closing the menu when a click is detected outside of the menu element.
+ **/
 const useToggleMenu = () => {
-  const [expanded, setExpanded] = useState(false);
-  const ref = useRef(null);
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-        if (ref.current && !ref.current.contains(event.target)) {
-            setExpanded(false);
-        }
-    }
-    document.addEventListener('mouseup', handleClickOutside);
-    return () => {
-        document.removeEventListener('mouseup', handleClickOutside);
-    };
-  }, [ref]);
+	const [expanded, setExpanded] = useState(false);
+	const ref = useRef(null);
+	useEffect(() => {
+		/**
+		 * Handles click events on the document.
+		 * Closes the menu if the click is outside the menu element.
+		 **/
+		const handleClickOutside = (event) => {
+			if (ref.current && !ref.current.contains(event.target)) {
+				setExpanded(false);
+			}
+		};
+		document.addEventListener("mouseup", handleClickOutside);
+		return () => {
+			document.removeEventListener("mouseup", handleClickOutside);
+		};
+	}, [ref]);
 
-  return { expanded, setExpanded, ref };
+	return { expanded, setExpanded, ref };
 };
 
 export default useToggleMenu;
