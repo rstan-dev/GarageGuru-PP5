3e7a939c97aae81ce833b9c4459fef632cc4b0e9
Russ-S <rstanleym74@gmail.com>
2023-12-04 05:52:49 +0000

    style Invoice Card dashboard
    Update AllInvoices page with dashboard styles from AllJobs Page.
    
    Update css module.

--- frontend/src/pages/invoices/AllInvoicesPage.js
+++ frontend/src/pages/invoices/AllInvoicesPage.js
@@ -7,6 +7,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 import styles from '../../styles/AllInvoicesPage.module.css'
 import InvoiceCard from './InvoiceCard';
@@ -72,89 +73,123 @@ function AllInvoicesPage() {
         // sets the ordering field state for created date, updated date
         // and due date
         setOrderingField(field);
-      };
+    };
+
+    const handleClearSearch = () => {
+        // Clears the query state when X button is clicked
+        setQuery('');
+    };
 
     return (
     <>
     <div>AllInvoicesPage</div>
 
-    <Container className={styles.InvoiceCard}>
+    <Container className={styles.AllInvoicesContainer}>
             <Col xs={12} sm={12} md={10} lg={10} xl={10}>
-                <div className={styles.CardBlock}>
+            <div className={styles.CardBlock}>
 
-                    {
-                    // Status Block //
-                    }
+                {/* Status Block */}
+                <Card className={styles.StatusCard}>
+                    <p className={styles.DashboardHeadings}>Dashboard</p>
 
-                    <p>Filter by:</p>
                     <div className={`row ${styles['StatusBlock']}`}>
-                        <div className="col-md-4"
-                            onClick={() => {setQuery("Pending"); setSelectedStatus("Pending");}}>
-                            <div className={`card ${styles.CardPointer} ${selectedStatus === 'Pending' ? styles['PendingBorder'] : ''}`}>
-                                <div className="card-body text-center">
-                                    <i className={`fa-solid fa-bell-concierge ${styles['PendingIcon']}`}></i>
-                                    <h2 className="card-title">Pending</h2>
-                                    {statusCounts.Pending ? (
-                                            <p className="card-text">{statusCounts.Pending}</p>
-                                        ) : (
-                                            <p className="card-text">0</p>
-                                        )}
+
+                                {/* Paid Status */}
+                                <div className="col-md-4">
+                                <div
+                                    className={`card ${styles.CardPointer} ${selectedStatus === 'Pending' ? styles['PendingBorder'] : ''}`}
+                                    onClick={() => {setQuery("Pending"); setSelectedStatus("Pending");}}>
+                                    <div className={`card-body text-center ${styles.StatusIconContainer}`}>
+                                        <i className={`fa-solid fa-bell-concierge ${styles['PendingIcon']}`}></i>
+                                        <div className={styles.Title}>Pending</div>
+                                        <div className={styles.Title}>{statusCounts.Pending || '0'}</div>
+                                    </div>
+                                </div>
+
+                                {/* Pending Selector Button */}
+                                <div className={styles.SelectorButton}>
+                                    <Button
+                                        onClick={() => { setQuery("Pending"); setSelectedStatus("Pending"); }}
+                                            variant="outline-danger"
+                                            size="sm"
+                                    >
+                                    Set to Pending
+                                        </Button>
                                 </div>
                             </div>
 
-                        </div>
-                        <div className="col-md-4"
-                            onClick={() => {setQuery("Invoiced"); setSelectedStatus("Invoiced");}}>
-                            <div className={`card ${styles.CardPointer} ${selectedStatus === 'Invoiced' ? styles['InvoicedBorder'] : ''}`}>
-                                <div className="card-body text-center">
+                        {/* Invoiced Status */}
+                        <div className="col-md-4">
+                            <div
+                                className={`card ${styles.CardPointer} ${selectedStatus === 'Invoiced' ? styles['InvoicedBorder'] : ''}`}
+                                onClick={() => {setQuery("Invoiced"); setSelectedStatus("Invoiced");}}>
+                                    <div className={`card-body text-center ${styles.StatusIconContainer}`}>
                                     <i className={`fa-solid fa-hourglass-half ${styles['InvoicedIcon']}`}></i>
-                                    <h2 className="card-title">Invoiced</h2>
-                                    {statusCounts.Invoiced ? (
-                                            <p className="card-text">{statusCounts.Invoiced}</p>
-                                        ) : (
-                                            <p className="card-text">0</p>
-                                        )}
+                                    <div className={styles.Title}>Invoiced</div>
+                                    <div className={styles.Title}>{statusCounts.Invoiced || '0'}</div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-md-4"
-                            onClick={() => {setQuery("Paid"); setSelectedStatus("Paid");}}>
-                            <div className={`card ${styles.CardPointer} ${selectedStatus === 'Paid' ? styles['PaidBorder'] : ''}`}>
-                                <div className="card-body text-center">
-                                <i className={`fa-solid fa-flag-checkered ${styles['PaidIcon']}`}></i>
-                                <h2 className="card-title">Paid</h2>
-                                    {statusCounts.Paid ? (
-                                        <p className="card-text">{statusCounts.Paid}</p>
-                                    ) : (
-                                        <p className="card-text">0</p>
-                                    )}
-                                </div>
+
+                            {/* Invoiced Selector Button */}
+                            <div className={styles.SelectorButton}>
+                                <Button
+                                    onClick={() => { setQuery("Invoiced"); setSelectedStatus("Invoiced"); }}
+                                        variant="outline-warning"
+                                        size="sm"
+                                >
+                                Set to Invoiced
+                                    </Button>
                             </div>
                         </div>
+
+
+                        {/* Paid Status */}
+                        <div className="col-md-4">
+                            <div
+                                className={`card ${styles.CardPointer} ${selectedStatus === 'Paid' ? styles['PaidBorder'] : ''}`}
+                                onClick={() => {setQuery("Paid"); setSelectedStatus("Paid");}}>
+                                <div className={`card-body text-center ${styles.StatusIconContainer}`}>
+                                    <i className={`fa-solid fa-hourglass-half ${styles['PaidIcon']}`}></i>
+                                    <div className={styles.Title}>Paid</div>
+                                    <div className={styles.Title}>{statusCounts.Paid || '0'}</div>
+                                </div>
+                            </div>
+
+                            {/* Paid Selector Button */}
+                            <div className={styles.SelectorButton}>
+                                <Button
+                                    onClick={() => { setQuery("Paid"); setSelectedStatus("Paid"); }}
+                                        variant="outline-success"
+                                        size="sm"
+                                >
+                                Set to Paid
+                                    </Button>
+                            </div>
+                        </div>
+
+                    {/* close status block div */}
                     </div>
 
-                    {
-                    // Order By Filter //
-                    }
-
-                    <p className="text-md-end">Order by:</p>
+                    {/* Order by section */}
+                    <p className={`text-md-end ${styles.DashboardHeadings}`}>Order by:</p>
                     <div className={`row ${styles.OrderBySection}`}>
 
-                        <div className="col-md-4">
-                            <Button onClick={() => handleOrderBy('-created_at')} variant="secondary">Recently Created</Button>
+                            <div className={`col-md-4 ${styles.OrderByBtnContainers}`}>
+                            <Button onClick={() => handleOrderBy('-created_at')} variant="outline-info" size="sm">Recently Created</Button>
                         </div>
-                        <div className="col-md-4">
-                            <Button onClick={() => handleOrderBy('-updated_at')} variant="secondary">Recently Updated</Button>
+                        <div className={`col-md-4 ${styles.OrderByBtnContainers}`}>
+                            <Button onClick={() => handleOrderBy('-updated_at')} variant="outline-info" size="sm">Recently Updated</Button>
                         </div>
-                        <div className="col-md-4">
-                            <Button onClick={() => handleOrderBy('due_date')} variant="secondary">Due Date</Button>
+                        <div className={`col-md-4 ${styles.OrderByBtnContainers}`}>
+                            <Button onClick={() => handleOrderBy('due_date')} variant="outline-info" size="sm">Due Date</Button>
                         </div>
+
                     </div>
 
 
-                    {
-                    // Search bar //
-                    }
+                    {/* Search bar section */}
+                    <p className={`text-md-end ${styles.DashboardHeadings}`}>Keyword search:</p>
+                    <div className={styles.SearchBarContainer}>
                     <i className={`fas fa-search ${styles.SearchIcon}`} />
                     <Form
                     className={styles.SearchBar}
@@ -163,11 +198,20 @@ function AllInvoicesPage() {
                         <Form.Control
                         type="text"
                         className="mr-sm-2"
-                        placeholder="Search invoices"
+                        placeholder="Search jobs"
                         value={query}
                         onChange={(event) => setQuery(event.target.value)}
                         />
+                        {query && (
+                            <div className={styles.ClearSearchButton} onClick={handleClearSearch}>
+                                <i className="fa-regular fa-circle-xmark"></i>
+                            </div>
+                        )}
                     </Form>
+                    </div>
+
+                </Card>
+            </div>
 
 
                     {
@@ -205,13 +249,12 @@ function AllInvoicesPage() {
                     icon={"fa-solid fa-clipboard-question"}
                     message={"No Jobs to display"}
                     />
-            )}
+                    )}
                     </>
                     ) : (
                         <Asset spinner message={"loading jobs"} />
                     )}
 
-                </div>
                 </Col>
     </Container>
     </>
