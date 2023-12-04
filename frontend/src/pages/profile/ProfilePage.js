5bf5b9decf5c92e24d0098df3c5aeeafe2084752
Russ-S <rstanleym74@gmail.com>
2023-12-04 15:04:18 +0000

    style: add style to ProfilePage
    Create css module.
    
    Update profile design, add classnames.

--- frontend/src/pages/profile/ProfilePage.js
+++ frontend/src/pages/profile/ProfilePage.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { Image } from "react-bootstrap";
+import styles from "../../styles/ProfilePage.module.css"
 
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -55,61 +57,95 @@ const ProfilePage = () => {
     }, [currentUserId, currentUser, history, id]);
 
     return (
-         <Container>
-      <Row>
-        <Col md={4}>
-          <div className="profile-image">
-            <h1>Profile Page For {owner} </h1>
-            <Image
-              src={image}
-              alt="Profile"
-              roundedCircle
-              fluid
-            />
-          </div>
-        </Col>
-        <Col md={8}>
-          <div className="profile-info">
-            <p>
-              Name: {name}
-            </p>
-            <p>
-              Bio: {bio}
-            </p>
-            <p>
-              Staff Member since: {created_at}
-            </p>
-            <p>
-              Profile updated on: {updated_at}
-            </p>
-            <p>User Id: {id}</p>
+      <Container className={styles.ProfileForm}>
+      <Col xs={12} sm={12} md={8} lg={8} xl={8} className="mx-auto">
 
-            {is_owner ? (
-              <>
-            <Link to={`/profile/${id}/edit-profile`}>
-              <Button
-                variant="warning">
-                Edit Profile
-              </Button>
-            </Link>
-            <Link to={`/profile/${id}/change-username`}>
-                <Button variant="warning">
-                  Edit Username
-                </Button>
-            </Link>
-            <Link to={`/profile/${id}/change-password`}>
-              <Button variant="warning">
-                Edit Password
-              </Button>
-            </Link>
-            </>
-            ) : (
-              null
-            )}
-          </div>
-        </Col>
-      </Row>
-    </Container>
+
+          <div className={styles.CardBlock}>
+              <Card className={styles.FormCard}>
+                  <div className={`d-flex flex-column align-items-center`}>
+                      <p>
+                        <i className={`fa-solid fa-circle-user ${styles.ProfileIcon}`}>
+                  </i>
+                  Profile Page for {owner}
+                      </p>
+                  </div>
+
+
+              <Card className={styles.ImageContainer}>
+              <Image
+                src={image}
+                alt="Profile"
+                fluid
+              />
+
+              </Card>
+
+              <table className="table table-striped">
+                    <tbody>
+                      <tr>
+                        <th><i className="fa-solid fa-id-card-clip"></i>Name:</th>
+                        <td>{name}</td>
+                  </tr>
+                  <tr>
+                        <th><i className="fa-solid fa-circle-info"></i>Bio:</th>
+                        <td>{bio}</td>
+                  </tr>
+                  <tr>
+                        <th><i className="fa-solid fa-calendar-days"></i>Staff member since:</th>
+                        <td>{created_at}</td>
+                  </tr>
+                  <tr>
+                        <th><i className="fa-solid fa-calendar-plus"></i>Profile last updated:</th>
+                        <td>{updated_at}</td>
+                  </tr>
+                  <tr>
+                        <th><i className="fa-solid fa-hashtag"></i>User ID:</th>
+                        <td>{id}</td>
+                  </tr>
+
+                </tbody>
+                </table>
+
+              {is_owner ? (
+                <>
+                  <Row className="justify-content-center">
+
+                    <Col md="auto" className={styles.BtnContainer}>
+                      <Link to={`/profile/${id}/edit-profile`}>
+                        <Button
+                          variant="primary">
+                          Edit Profile
+                        </Button>
+                      </Link>
+                    </Col>
+
+                    <Col md="auto" className={styles.BtnContainer}>
+                      <Link to={`/profile/${id}/change-username`}>
+                        <Button variant="warning">
+                          Edit Username
+                        </Button>
+                      </Link>
+                    </Col>
+
+                    <Col md="auto" className={styles.BtnContainer}>
+                      <Link to={`/profile/${id}/change-password`}>
+                        <Button variant="danger">
+                          Edit Password
+                        </Button>
+                      </Link>
+                    </Col>
+                  </Row>
+                </>
+              ) : (
+                null
+              )}
+
+
+  </Card>
+  </div>
+  </Col>
+</Container>
   );
 };
 
