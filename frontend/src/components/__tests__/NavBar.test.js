30789b2e14b1d4e30c59b037e029a724fb572965
Russ-S <rstanleym74@gmail.com>
2023-12-06 15:40:19 +0000

    test: renders NavBar Add Job link.

--- frontend/src/components/__tests__/NavBar.test.js
+++ frontend/src/components/__tests__/NavBar.test.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom/cjs/react-router-dom.min";
 import NavBar from "../NavBar";
+import * as UserContext from '../../contexts/CurrentUserContext';
 
+jest.mock('../../contexts/CurrentUserContext', () => ({
+    useCurrentUser: jest.fn(),
+    useSetCurrentUser: jest.fn(),
+  }));
 
 test('renders NavBar', () => {
     render(<Router>
@@ -25,4 +30,22 @@ test('Renders Register link', () => {
     expect(registerLink).toBeInTheDocument();
 });
 
+describe('NavBar Component', () => {
+    it('renders Add Job link when user is logged in', () => {
+      // Setup mock return values
+      UserContext.useCurrentUser.mockReturnValue({ pk: 1, username: 'testuser' });
+      UserContext.useSetCurrentUser.mockReturnValue(jest.fn());
 
+      // Render the NavBar component
+      render(
+        <Router>
+          <NavBar />
+        </Router>
+      );
+
+      // Assert that the Add Job link is in the document
+      const addJobLink = screen.getByRole('link', { name: 'Add Job' });
+      expect(addJobLink).toBeInTheDocument();
+    });
+
+  });
