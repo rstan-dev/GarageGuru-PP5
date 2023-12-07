96d8b889d72b8bcd80beabfec135d683158855c1
Russ-S <rstanleym74@gmail.com>
2023-12-07 12:35:20 +0000

    test: GG281123: Edit Invoice Details
    Create new custom permission class for IsOwnerOrAssignedToOrReadOnly.
    
    Update InvoiceDetail View with custom permission class.
    
    Update state and InvoiceData props to use inv_owner_id and job_assigned_to_id for clarity in EditInvoiceForm.
    
    Update currentUser conditional with new props and move to handleSubmit function to perform permission check.
    
    Test edit invoice functionality: bob-brown invoice edited by assigned_user duffyweho.

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -2,6 +2,42 @@
 Imports for Permissions
 """
 from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Allows all users Read Only access and controls access to specific
+    objects based on whether the user making the request is the owner
+    of the object or not.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
+
+
+class IsOwnerOrAssignedToOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object or users assigned
+    to the job to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request,
+        if request.method in SAFE_METHODS:
+            return True
+
+        is_owner = obj.owner == request.user
+
+        # Checks if the user is assigned to the job
+        is_assigned_to_job = obj.job and (
+            obj.job.assigned_to == request.user
+            or obj.job.assigned_to_id == request.user.id
+        )
+
+        return is_owner or is_assigned_to_job
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
--- frontend/src/pages/invoices/EditInvoiceForm.js
+++ frontend/src/pages/invoices/EditInvoiceForm.js
@@ -33,7 +33,9 @@ function EditInvoiceForm() {
 	const [invoiceData, setInvoiceData] = useState({
 		inv_id: "",
 		inv_owner: "",
+		inv_owner_id: "",
 		job_assigned_to: "",
+		job_assigned_to_id: "",
 		job_id: null,
 		customer_firstname: "",
 		customer_lastname: "",
@@ -47,7 +49,9 @@ function EditInvoiceForm() {
 	const {
 		inv_id,
 		inv_owner,
+		inv_owner_id,
 		job_assigned_to,
+		job_assigned_to_id,
 		job_id,
 		customer_firstname,
 		customer_lastname,
@@ -80,7 +84,9 @@ function EditInvoiceForm() {
 				setInvoiceData({
 					inv_id: data.inv_id,
 					inv_owner: data.inv_owner,
+					inv_owner_id: data.inv_owner_id,
 					job_assigned_to: data.job_assigned_to,
+					job_assigned_to_id: data.job_assigned_to_id,
 					job_id: data.job_id,
 					customer_firstname: data.customer_firstname,
 					customer_lastname: data.customer_lastname,
@@ -91,21 +97,13 @@ function EditInvoiceForm() {
 					invoice_status: data.invoice_status,
 				});
 
-				if (
-					currentUser &&
-					currentUser.username !== data.inv_owner &&
-					data.currentUser.username !== job_assigned_to
-				) {
-					// Redirect to home page if currentUser is neither invoice owner nor assigned user.
-					history.push("/");
-					return;
-				}
 			} catch (err) {
 				console.log(err);
 			}
 		};
 		handleMount();
-	}, [history, id, currentUser, inv_owner, job_assigned_to]);
+	}, [history, id, currentUser, inv_owner, job_assigned_to, inv_owner_id,
+		job_assigned_to_id,]);
 
 	/**
 	 * Handles changes to any of the form fields and updates the corresponding state.
@@ -134,6 +132,15 @@ function EditInvoiceForm() {
 	 **/
 	const handleSubmit = async (event) => {
 		event.preventDefault();
+
+		// Checks if the current user has permission to edit the invoice
+		if (
+			currentUser.pk !== inv_owner_id &&
+			currentUser.pk !== job_assigned_to_id
+		) {
+			history.push("/");
+			return;
+		}
 
 		let formErrors = {};
 
