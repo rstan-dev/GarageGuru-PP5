1522e908b780ec7981ff7664fad44aed3424a75a
Russ-S <rstanleym74@gmail.com>
2023-12-08 12:33:57 +0000

    style: format EditComment Form for ES8

--- frontend/src/pages/comments/EditCommentForm.js
+++ frontend/src/pages/comments/EditCommentForm.js
@@ -221,7 +221,6 @@ const EditCommentForm = (props) => {
 						onClick={() => setDisplayEditForm()}
 						type='button'
 						className={styles.ButtonSpacing}>
-
 						Cancel
 					</Button>
 					<Button
