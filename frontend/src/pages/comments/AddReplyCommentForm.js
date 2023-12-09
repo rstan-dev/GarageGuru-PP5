efcdc827538f93f67d3c372365c9181e1061d46d
Russ-S <rstanleym74@gmail.com>
2023-12-09 10:14:17 +0000

    fix:  add success and alert message to AddReplyComments
    Add state for successMessage and errors.
    
    Add errorKey state to trigger refresh after error display.
    
    Set isMountedRef.
    
    Refactor handleSubmit to handle errors and sucessMessage with time out.
    
    Insert errors to form field.
    
    Test error message displays as expected when submitting an empty reply comment.

--- frontend/src/pages/comments/AddReplyCommentForm.js
+++ frontend/src/pages/comments/AddReplyCommentForm.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import Alert from "react-bootstrap/Alert";
+import TimedAlert from "../../components/TimedAlert";
 import styles from "../../styles/AddEditComment.module.css";
 
 import { axiosRes } from "../../api/axiosDefaults";
@@ -17,8 +19,15 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 function AddReplyCommentForm(props) {
 	const { jobId, id, setComments, setCommentsCount } = props;
 
-	// State for managing the input of the new reply comment.
+	// State for managing the input of the new reply comment, and success
+	// messages, errors, and error key to trigger update.
 	const [comment_detail, setComment_detail] = useState("");
+	const [successMessage, setSuccessMessage] = useState("");
+	const [errors, setErrors] = useState({});
+	const [errorKey, setErrorKey] = useState(0);
+
+	const successTimeoutRef = useRef();
+	const isMountedRef = useRef(true);
 
 	// Parent comment ID and job ID for the reply.
 	const parent = id;
@@ -51,6 +60,23 @@ function AddReplyCommentForm(props) {
 			return;
 		}
 
+		let formErrors = {};
+
+		if (
+			!comment_detail ||
+			comment_detail === "add a reply to this comment..."
+		) {
+			formErrors.comment_detail = [
+				"A comment is required. Please add a comment.",
+			];
+		}
+
+		if (Object.keys(formErrors).length > 0) {
+			setErrorKey((prevKey) => prevKey + 1);
+			setErrors(formErrors);
+			return;
+		}
+
 		try {
 			const payload = {
 				comment_detail,
@@ -72,37 +98,56 @@ function AddReplyCommentForm(props) {
 				parent: data.parent,
 			};
 
-			setComments((prevComments) => {
-				// Map through the existing comments, add the replies to comment id
-				// that matches the parent id.
-				const updatedComments = prevComments.results.map((comment) => {
-					if (comment.id === parent) {
-						const updatedReplies = [formattedReply, ...comment.replies];
-						return {
-							...comment,
-							replies: updatedReplies,
-						};
-					}
-					return comment;
+			if (isMountedRef.current) {
+				setComments((prevComments) => {
+					// Map through the existing comments, add the replies to comment id
+					// that matches the parent id.
+					const updatedComments = prevComments.results.map((comment) => {
+						if (comment.id === parent) {
+							const updatedReplies = [formattedReply, ...comment.replies];
+							return {
+								...comment,
+								replies: updatedReplies,
+							};
+						}
+						return comment;
+					});
+
+					// Return the updated state.
+					return {
+						...prevComments,
+						results: updatedComments,
+					};
 				});
 
-				// Return the updated state.
-				return {
-					...prevComments,
-					results: updatedComments,
-				};
-			});
+				// Increments the comments count and resets the input field.
+				setCommentsCount((prevCount) => prevCount + 1);
 
-			// Increments the comments count and resets the input field.
-			setCommentsCount((prevCount) => prevCount + 1);
-			setComment_detail("");
+				// Sets the success message with timeout
+				setSuccessMessage("Comment has been added successfully");
+				successTimeoutRef.current = setTimeout(() => {
+					setSuccessMessage("");
+				}, 1500);
+
+				// Reset the comment input field
+				setComment_detail("");
+			}
 		} catch (err) {
 			console.log(err);
+			setErrors({ message: ["There was an error submitting the form."] });
 		}
 	};
 
 	return (
 		<div className={styles.ReplyCommentArea}>
+			{/* Display success message */}
+			{successMessage && (
+				<Alert
+					className={styles.SuccessMessage}
+					variant='success'>
+					{successMessage}
+				</Alert>
+			)}
 			<div className='row'>
 				<div className={`col ${styles.ProfileSection}`}>
 					<Image
@@ -125,6 +170,16 @@ function AddReplyCommentForm(props) {
 								className={styles.FormControl}
 							/>
 						</Form.Group>
+						<div key={errorKey}>
+							{errors?.comment_detail?.map((message, index) => (
+								<TimedAlert
+									key={index}
+									message={message}
+									variant='warning'
+									timeout={3000}
+								/>
+							))}
+						</div>
 						<div className={`text-right ${styles.SubmitButton}`}>
 							<Button
 								variant='outline-success'
