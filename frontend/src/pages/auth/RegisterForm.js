0faf71e00bf01c3a3bf4570f170427811261e047
Russ-S <rstanleym74@gmail.com>
2023-12-09 11:00:43 +0000

    ehancement: Add TimedAlert on RegisterForm
    Import TimedAlert, setErrorKey state. Update handleSubmit with error handling.
    
    Update form with TimedAlert component.
    
    Test empty form submission validates as expected.

--- frontend/src/pages/auth/RegisterForm.js
+++ frontend/src/pages/auth/RegisterForm.js
@@ -6,7 +6,8 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
-import { Alert } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
+import TimedAlert from "../../components/TimedAlert";
 
 import styles from "../../styles/LoginRegister.module.css";
 import axios from "axios";
@@ -28,8 +29,9 @@ function RegisterForm() {
 
 	const { username, password1, password2 } = registerData;
 
-	// State for managing form errors.
+	// State for managing form errors and error key to trigger update.
 	const [errors, setErrors] = useState({});
+	const [errorKey, setErrorKey] = useState(0);
 
 	// State for managing the success message post-registration.
 	const [successMessage, setSuccessMessage] = useState("");
@@ -47,10 +49,31 @@ function RegisterForm() {
 
 	const handleSubmit = async (event) => {
 		/**
-		 * Handles form submission, sets and displays a success message
+		 * Handles form submission, sets and displays errors and a success message
 		 * before redirecting the user to the login page
 		 **/
 		event.preventDefault();
+
+		let formErrors = {};
+
+		if (!username || username === "Enter a username") {
+			formErrors.username = ["Please choose a username..."];
+		}
+
+		if (!password1 || password1 === "Enter a password at least 8 characters") {
+			formErrors.password1 = ["Please choose a password..."];
+		}
+
+		if (!password2 || password2 === "Re-enter the same password") {
+			formErrors.password2 = ["Please confirm using the same password..."];
+		}
+
+		if (Object.keys(formErrors).length > 0) {
+			setErrorKey((prevKey) => prevKey + 1);
+			setErrors(formErrors);
+			return;
+		}
+
 		try {
 			await axios.post("dj-rest-auth/registration/", registerData);
 			setSuccessMessage(
@@ -75,7 +98,13 @@ function RegisterForm() {
 				xl={6}
 				className='mx-auto'>
 				{/* Display success message */}
-				{successMessage && <Alert className={styles.SuccessMessage} variant='success'>{successMessage}</Alert>}
+				{successMessage && (
+					<Alert
+						className={styles.SuccessMessage}
+						variant='success'>
+						{successMessage}
+					</Alert>
+				)}
 
 				<div className={styles.CardBlock}>
 					<Card className={styles.FormCard}>
@@ -83,8 +112,8 @@ function RegisterForm() {
 							className={`d-flex flex-column align-items-center ${styles.Intro}`}>
 							<h1>
 								<i
-									className={`fa-solid fa-solid fa-user-plus ${styles.LoginRegisterIcon}`} aria-hidden='true'>
-								</i>
+									className={`fa-solid fa-solid fa-user-plus ${styles.LoginRegisterIcon}`}
+									aria-hidden='true'></i>
 								Register for an Account
 							</h1>
 							<p>Sign up to GarageGuru, the dynamic job card management app.</p>
@@ -103,13 +132,16 @@ function RegisterForm() {
 							</Form.Group>
 
 							{/* Display error message */}
-							{errors.username?.map((message, index) => (
-								<Alert
-									key={index}
-									variant='warning'>
-									{message}
-								</Alert>
-							))}
+							<div key={`username-errors-${errorKey}`}>
+								{errors.username?.map((message, index) => (
+									<TimedAlert
+										key={index}
+										message={message}
+										variant='warning'
+										timeout={3000}
+									/>
+								))}
+							</div>
 
 							<Form.Group controlId='password1'>
 								<Form.Label>Choose Password</Form.Label>
@@ -124,13 +156,16 @@ function RegisterForm() {
 							</Form.Group>
 
 							{/* Display error message */}
-							{errors.password1?.map((message, index) => (
-								<Alert
-									key={index}
-									variant='warning'>
-									{message}
-								</Alert>
-							))}
+							<div key={`password1-errors-${errorKey}`}>
+								{errors.password1?.map((message, index) => (
+									<TimedAlert
+										key={index}
+										message={message}
+										variant='warning'
+										timeout={3000}
+									/>
+								))}
+							</div>
 
 							<Form.Group controlId='password2'>
 								<Form.Label>Confirm Password</Form.Label>
@@ -145,13 +180,16 @@ function RegisterForm() {
 							</Form.Group>
 
 							{/* Display error message */}
-							{errors.password2?.map((message, index) => (
-								<Alert
-									key={index}
-									variant='warning'>
-									{message}
-								</Alert>
-							))}
+							<div key={`password2-errors-${errorKey}`}>
+								{errors.password2?.map((message, index) => (
+									<TimedAlert
+										key={index}
+										message={message}
+										variant='warning'
+										timeout={3000}
+									/>
+								))}
+							</div>
 
 							<Row className='justify-content-center'>
 								<Col
