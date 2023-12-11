79ccad36a219e081d44ae4366acb6df751a75d51
Russ-S <rstanleym74@gmail.com>
2023-12-11 17:11:17 +0000

    [US34] deployment settings
    Add Root route to drf_api urls
    
    Add DEV conditional to render JSON data in settings.py.

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from .views import root_route, logout_route
 
 urlpatterns = [
+    path("", root_route),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("dj-rest-auth/logout/", logout_route),
