b0845ae095638705913457b5aa006d9b28043846
Russ-S <rstanleym74@gmail.com>
2023-12-11 21:18:22 +0000

    fix: typo in axiosDefaults
    Rebuild static files and run build.

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -8,7 +8,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "/api";
-axios.defaults.headers.post['Content-type'] = 'multipart/form-data';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
