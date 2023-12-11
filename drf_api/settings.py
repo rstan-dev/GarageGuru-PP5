63e3ab015b33f5e6cb6813519e0cb537c7eec81e
Russ-S <rstanleym74@gmail.com>
2023-12-11 18:43:49 +0000

    [US34] update settings and files for deployment
    Install gunicorn, install whitenoise, add Procfile, update requirements.txt
    
    Update settings.py:  add Heroku url to ALLOWED_HOST, add whitenoise settings, update INSTALLED_APPS.
    
    Update drf_api urls.py with TemplateView - replace root_route.  Update urls to include api/.
    
    Update axiosDefaults.js - add baseURL.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,11 +59,12 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEBUG" in os.environ
+DEBUG = "DEV" in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get("ALLOWED_HOST"),
-    "localhost",
+    'localhost',
+    'garageguru2023-9cc8e49ac2b8.herokuapp.com'
 ]
 
 
@@ -75,8 +76,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "cloudinary_storage",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
     "cloudinary",
     "rest_framework",
     "django_filters",
@@ -100,6 +101,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -117,7 +119,7 @@ ROOT_URLCONF = "drf_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'staticfiles', 'build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -189,6 +191,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 MEDIA_URL = "/media/"
 
--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -7,8 +7,8 @@
 
 import axios from "axios";
 
-// axios.defaults.baseURL = 'https://8000-rstandev-garagegurupp5-g87jnb0h38p.ws-eu105.gitpod.io/';
-// axios.defaults.headers.post['Content-type'] = 'multipart/form-data';
+axios.defaults.baseURL = "/api";
+axios.defaults.headers.post['Content-type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
